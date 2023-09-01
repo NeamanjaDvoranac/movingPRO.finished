83420f9a43dc016b6b5eac9b55fc3cfc8bd0e892
NeamanjaDvoranac <90352223+NeamanjaDvoranac@users.noreply.github.com>
2023-09-01 21:22:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let lastScrollpos = window.pageYOffset;
+let menuButtonCheck = document.querySelector(".menu-btn");
+window.onscroll = function() {
+	let currentScrollPos = window.pageYOffset;
+	if (lastScrollpos > currentScrollPos) {
+		document.querySelector(".top-nav").style.top = "0";
+	} else {
+		document.querySelector(".top-nav").style.top = "-75px";
+	}
+	lastScrollpos = currentScrollPos;
+	menuButtonCheck.checked = false;
+} 
+
+
+
+  
+
+
